f2b6a9e566a007137e9da42aebd247d0ad2b00e0
Juan Diego Porras <jporras@iqtsystems.com>
2024-03-21 16:56:29 -0400

    implementada funcionalidad de borrado de elementos completados

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Router } from '@angular/router';
-import { FilterList } from 'src/app/core/models/item.model';
+import { FilterList, Item } from 'src/app/core/models/item.model';
+import { ItemsService } from 'src/app/items/services/items.service';
 
 @Component({
   selector: 'app-footer',
@@ -12,7 +13,7 @@ export class FooterComponent  {
  @Input() filter: FilterList = "all";
  @Output() updateFilter = new EventEmitter<FilterList>();
 
- constructor(private router: Router) {
+ constructor(private router: Router, private itemsService: ItemsService) {
   
  }
 
@@ -22,6 +23,11 @@ export class FooterComponent  {
   this.updateFilter.next(this.filter);
   console.log("vine para el footer component");
   this.router.navigateByUrl(`/${this.filter}`)
+ }
+
+ clearCompletedItems(): void {
+   let itemsAux = this.itemsService.getFilteredItems("pending");
+   this.itemsService.setItemsList(itemsAux);
  }
 
 
